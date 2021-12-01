534fde84444c8baea1e03b13b83d13e357925f76
ErinYeet <95351909+ErinYeet@users.noreply.github.com>
2021-12-01 11:36:40 +0000

    Add files via upload

---
+++ day1/code.py
@@ -0,0 +1,27 @@
+
+
+f = open("input.txt", "r")
+lines = f.readlines()
+lines = [line.rstrip() for line in lines]
+print(lines)
+
+intlines = [int(line) for line in lines]
+silvercount = 0
+
+for x in range(1,len(intlines)):
+    if intlines[x] > intlines[x-1]:
+        silvercount = silvercount+1
+
+print(silvercount)
+
+goldcount = 0
+
+rollingsums = [sum(intlines[x:x+3]) for x in range(len(intlines)-2)]
+
+print(rollingsums)
+
+for x in range(1,len(rollingsums)):
+    if rollingsums[x] > rollingsums[x-1]:
+        goldcount = goldcount+1
+
+print(goldcount)
